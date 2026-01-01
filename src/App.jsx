60be80392ae5669e0b1db2ed3babf287a93c544a
Siri Mahalaxmi Vemula <sirimahalaxmivemula@gmail.com>
2026-01-01 13:52:05 +0530

    changing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
-import Home from "./components/Home";
-import Loading from "./pages/Loading";
-import Bootstrap from "./pages/Bootstrap";
 import PhaseOne from "./components/PhaseOne";
+import Home from "./components/Home";
 import Player from "./components/Player";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/bootstrap" element={<Bootstrap />} />
-      <Route path="/loading" element={<Loading />} />
+      <Route path="/" element={<PhaseOne />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/home" element={<Home />} />
-       <Route path="/" element={<PhaseOne />} />
-       <Route path="/player" element={<Player />} />
+      <Route path="/player" element={<Player />} />
     </Routes>
   );
 }
--- src/components/PhaseOne.jsx
+++ src/components/PhaseOne.jsx
@@ -18,7 +18,7 @@ export default function PhaseOne() {
       {/* CTA */}
       <button
         className="phase1-btn"
-        onClick={() => navigate("/home")}
+        onClick={() => navigate("/player")}
       >
         Feel Echoa
       </button>
